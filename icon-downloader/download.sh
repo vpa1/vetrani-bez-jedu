81877099900be29502f8e6c1ccb2ff5cf0881bef
Vaclav Parchansky <vparchansky@gmail.com>
2019-09-17 20:25:35 +0200

    Fix downloader issues

--- icon-downloader/download.sh
+++ icon-downloader/download.sh
@@ -5,7 +5,7 @@ POSTGRES_HOSTNAME=db
 OUTDIR=`mktemp -d`
 die() { echo "$*" 1>&2 ; exit 1; }
 #calculate the closest complete run
-echo $TIME
+echo $DATE $TIME Starting load 1>&2
 #Forecast type - 78 hours or 30 hours
 HOURS=
 if [ "$TIME" -lt "0045" ]; then 
@@ -13,7 +13,7 @@ if [ "$TIME" -lt "0045" ]; then
     HOURS=78
     REFTIME="18"
 elif [ "$TIME" -lt "0345" ]; then
-    REFDATE=$DATE
+    REFDATE=`date +%Y%m%d -d yesterday`
     HOURS=30
     REFTIME="21"
 elif [ "$TIME" -lt "0645" ]; then
@@ -49,21 +49,25 @@ fi
 
 #download the latest run datum
 #Download and process all the requested data into csv
+    echo Loading $REFDATE$REFTIME data 1>&2
 for ((i=0; i<=$HOURS; i++)); do 
     printf -v TIMEPOS "%03d" $i
     CSVU=$OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.csv
     CSVV=$OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.csv
-    (curl https://opendata.dwd.de/weather/nwp/icon-eu/grib/$REFTIME/u_10m/icon-eu_europe_regular-lat-lon_single-level_$REFDATE$REFTIME\_$TIMEPOS\_U_10M.grib2.bz2 | bunzip2 > $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.grib2 && 
+    (curl -s https://opendata.dwd.de/weather/nwp/icon-eu/grib/$REFTIME/u_10m/icon-eu_europe_regular-lat-lon_single-level_$REFDATE$REFTIME\_$TIMEPOS\_U_10M.grib2.bz2 | bunzip2 > $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.grib2 && 
     ./wgrib2 $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.grib2 -s -undefine out-box 11.8606258:19.1775203 48.5031603:51.11666 -csv $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.csv &&
     psql -c "\copy stage from '$CSVU' delimiter ','" -h $POSTGRES_HOSTNAME -U wforecast_app  wforecast &&
-    curl https://opendata.dwd.de/weather/nwp/icon-eu/grib/$REFTIME/v_10m/icon-eu_europe_regular-lat-lon_single-level_$REFDATE$REFTIME\_$TIMEPOS\_V_10M.grib2.bz2 | bunzip2 > $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.grib2 &&
+    curl -s https://opendata.dwd.de/weather/nwp/icon-eu/grib/$REFTIME/v_10m/icon-eu_europe_regular-lat-lon_single-level_$REFDATE$REFTIME\_$TIMEPOS\_V_10M.grib2.bz2 | bunzip2 > $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.grib2 &&
     ./wgrib2 $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.grib2 -s -undefine out-box 11.8606258:19.1775203 48.5031603:51.11666 -csv $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.csv &&
     psql -c "\copy stage from '$CSVV' delimiter ','" -h $POSTGRES_HOSTNAME -U wforecast_app  wforecast ) || die "Download or stage load failed."
     rm $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.grib2 $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.grib2
     rm $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_U_10M.csv
     rm $OUTDIR/$REFDATE$REFTIME\_$TIMEPOS\_V_10M.csv
+    echo Data for $REFDATE$REFTIME\_$TIMEPOS loaded and saved into stage. 1>&2
 done
 rm -fR $OUTDIR
 
 # run processing sql to load data into primary table to be available to the WS
+echo Copying data from stage. 1>&2
 (psql -c "SELECT fill_forecasts()" -h $POSTGRES_HOSTNAME -U wforecast_app  wforecast ) || die "Exec stored procedure failed."
+echo Data copied from stage 1>&2
